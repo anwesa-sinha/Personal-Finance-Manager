e03c8e8fdc11cf5d088f48cd470e2b0eb8b3d4b0
Anwesa Sinha <110493614+anwesa-sinha@users.noreply.github.com>
2023-06-10 01:27:58 +0530

    Delete Innovation-Project directory

--- Innovation-Project/front.js
+++
@@ -1,7 +0,0 @@
-function openForm() {
-    document.getElementById("popup-form").style.display = "block";
-}
-  
-function closeForm() {
-    document.getElementById("popup-form").style.display = "none";
-}
