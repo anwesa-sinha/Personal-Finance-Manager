c70de60c3f79ed214467920708ce27dba695e60b
Anwesa Sinha <110493614+anwesa-sinha@users.noreply.github.com>
2023-06-10 01:28:49 +0530

    reuploaded files.
    everything working fine.

---
+++ front.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("popup-form").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("popup-form").style.display = "none";
+}
